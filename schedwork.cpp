8d94bb8746afb23efe4e656b0fbc9c6e01b5e854
Egor Cherkashin <cherkash@usc.edu>
2022-12-02 01:34:36 +0000

    final

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,14 +19,14 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
-
+bool scheduleHelper(int row, int col, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched);
+bool checkIfValid(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, Worker_T id);
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
     const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
+    const size_t dailyNeed, // d
+    const size_t maxShifts, // m
     DailySchedule& sched
 )
 {
@@ -35,9 +35,67 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    // sched is a NxD matrix
 
-
-
+    //Constraints
+    //Each person (avail[Day][Worker]) can only work m times
+    std::vector<int> workCounter(avail[0].size(), 0);
+    for(int i = 0; i < avail.size(); i++){
+        vector<Worker_T> temp;
+        sched.push_back(temp);
+        for(int j = 0; j < dailyNeed; j++){
+            sched[i].push_back(INVALID_ID);
+        }
+    }
+    return scheduleHelper(0, 0, avail, dailyNeed, maxShifts, sched);
 
 }
 
+bool scheduleHelper(int row, int col, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched){
+    int n = int(avail.size());
+    if(row == n - 1 && col == dailyNeed){
+        return true;
+    }
+    if(col == dailyNeed){
+        row += 1;
+        col = 0;
+    }
+    for(Worker_T id = 0; id < Worker_T(int(avail[0].size())); id++){
+        sched[row][col] = id;
+        bool temp = checkIfValid(avail, dailyNeed, maxShifts, sched, id);
+        if(temp){
+            bool nxt = scheduleHelper(row, col+1, avail, dailyNeed, maxShifts, sched);
+            if(nxt){
+                return true;
+            }
+        }
+        sched[row][col] = INVALID_ID;
+    }
+    return false;
+}
+
+bool checkIfValid(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, Worker_T id){
+    int d = static_cast<int>(dailyNeed);
+    int m = static_cast<int>(maxShifts);
+    int n = static_cast<int>(avail.size());
+    int total_work = 0;
+    for(int i = 0; i < n; i++){
+        int daily_work = 0;
+        for(int j = 0; j < d; j++){
+            if(sched[i][j] == id && id != INVALID_ID){
+                if(avail[i][int(id)] == 0){
+                    return false;
+                }
+                daily_work += 1;
+                total_work += 1;
+            }
+        }
+        if(daily_work > 1){
+            return false;
+        }
+    }
+    if(total_work > m){
+        return false;
+    }
+    return true;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+std::set<std::string>& rec_help(const std::string& in, const std::string& floating, const std::set<std::string>& dict, std::string word, std::set<std::string>& output);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -18,8 +18,39 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+    std::set<std::string> output;
+    std::string word = "";
+    return rec_help(in, floating, dict, word, output);
 }
 
 // Define any helper functions here
+std::set<std::string>& rec_help(const std::string& in, const std::string& floating, const std::set<std::string>& dict, std::string word, std::set<std::string>& output){
+    //Have full word
+    if(in.size() == word.size()){
+        //If not in dict -> end
+        if(dict.find(word) == dict.end()){
+            return output;
+        }
+        if(floating.size() != 0){
+            return output;
+        }
+        //Otherwise all good
+        output.insert(word);
+        return output;
+    }
+    if(in.size() - word.size() < floating.size()){
+        return output;
+    }
+    //If the current character is not '-' no need to itterate
+    if(in[word.size()] != '-'){
+        return rec_help(in, floating, dict, word+in[word.size()], output);
+    }
+    for(char i = 'a'; i <= 'z'; i++){
+        std::string floating_copy = floating;
+        if(floating_copy.find(i) != std::string::npos){
+            floating_copy.erase(floating_copy.find(i), 1);
+        }
+        rec_help(in, floating_copy, dict, word+i, output);
+    }
+    return output;
+}
